4a8dd5f604afafa437ce16471f2f61b88995ae0b
flor <redmoon@redmoon.coopeksita.net>
2021-05-06 20:23:17 -0300

    wip

--- src/Components/Collages/CollagesContainer.js
+++ src/Components/Collages/CollagesContainer.js
@@ -1,9 +1,17 @@
 import React from 'react';
+import styled from 'styled-components'
+import temporaryCollage from '../../assets/img/collage.png';
+
+
+const TemporaryCollage = styled.img`
+    width: 100%;
+`; 
+
 
 const Collage = () => {
     return (
         <div>
-            
+            <TemporaryCollage src={temporaryCollage} />
         </div>
     );
 };
